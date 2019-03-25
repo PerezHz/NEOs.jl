cf7cadfe92e95e679270571e9524f0e246cb1f21
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-03-25 00:27:22 -0600

    Fix initialcond and other minor changes

--- main.jl
+++ main.jl
@@ -9,16 +9,15 @@ const nyears = 24.0 # since t0 is 2008-9-24, this ends the integration on 2032-9
 const radarobs = true
 
 #integrator warmup
-main(2, newtoniter, nyears, output=false, radarobs=radarobs)
-# main(2, newtoniter)
+main(1, newtoniter, nyears, output=false, radarobs=radarobs)
 println("*** Finished warmup")
 
 #root-finding methods warmup (integrate until first root-finding event):
-main(100, newtoniter, nyears, radarobs=radarobs)
-println("*** Finished root-finding warmup")
+# main(100, newtoniter, nyears, radarobs=radarobs)
+# println("*** Finished root-finding warmup")
 
-# main(300, newtoniter, nyears, radarobs=radarobs)
-# println("*** Finished root-finding test: several roots")
+main(300, newtoniter, nyears, radarobs=radarobs)
+println("*** Finished root-finding test: several roots")
 
 #Full jet transport integration until ~2038: about 8,000 steps
 # main(8000, newtoniter, nyears, radarobs=radarobs)
