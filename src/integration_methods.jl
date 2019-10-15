d0422bcb13260528093a10c30e813eef9da46ad7
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-10-15 12:21:38 -0500

    Fix root-finding integration method

--- src/integration_methods.jl
+++ src/integration_methods.jl
@@ -87,38 +87,33 @@ function apophisinteg(f!, q0::Array{U,1}, t0::T, tmax::T, order::Int, abstol::T,
 end
 
 # root-finding integration method
-function apophisinteg(f!, g, q0::Array{U,1}, trange::AbstractVector{T},
-        order::Int, abstol::T, params = nothing; maxsteps::Int=500, parse_eqs::Bool=true,
-        eventorder::Int=0, newtoniter::Int=10, nrabstol::T=eps(T)) where {T <: Real,U <: Number}
+function apophisinteg(f!, g, q0::Array{U,1}, t0::T, tmax::T, order::Int,
+        abstol::T, params = nothing; maxsteps::Int=500, parse_eqs::Bool=true,
+        dense::Bool=false, eventorder::Int=0, newtoniter::Int=10,
+        nrabstol::T=eps(T)) where {T <: Real,U <: Number}
 
     @assert order ≥ eventorder "`eventorder` must be less than or equal to `order`"
 
     # Allocation
-    nn = length(trange)
+    tv = Array{T}(undef, maxsteps+1)
     dof = length(q0)
-    x0 = similar(q0, eltype(q0), dof)
-    fill!(x0, T(NaN))
-    xv = Array{eltype(q0)}(undef, dof, nn)
-    for ind in 1:nn
-        @inbounds xv[:,ind] .= x0
+    xv = Array{U}(undef, dof, maxsteps+1)
+    if dense
+        xv_interp = Array{Taylor1{U}}(undef, dof, maxsteps+1)
     end
 
     # Initialize the vector of Taylor1 expansions
-    t = Taylor1( T, order )
+    t = Taylor1(T, order)
     x = Array{Taylor1{U}}(undef, dof)
     dx = Array{Taylor1{U}}(undef, dof)
     xaux = Array{Taylor1{U}}(undef, dof)
-    for i in eachindex(q0)
-        @inbounds x[i] = Taylor1( q0[i], order )
-        @inbounds dx[i] = Taylor1( zero(q0[i]), order )
-    end
+    dx .= Taylor1.(zeros(U, dof), order)
 
     # Initial conditions
-    @inbounds t[0] = trange[1]
-    @inbounds t0, t1, tmax = trange[1], trange[2], trange[end]
-    x0 = deepcopy(q0)
-    x1 = similar(x0)
+    @inbounds t[0] = t0
     x .= Taylor1.(q0, order)
+    x0 = deepcopy(q0)
+    @inbounds tv[1] = t0
     @inbounds xv[:,1] .= q0
 
     # Determine if specialized jetcoeffs! method exists
@@ -150,24 +145,14 @@ function apophisinteg(f!, g, q0::Array{U,1}, trange::AbstractVector{T},
     gvS = similar(tvS)
 
     # Integration
-    iter = 2
     nsteps = 1
     nevents = 1 #number of detected events
     while t0 < tmax
         δt_old = δt
         δt = apophisstep!(f!, t, x, dx, xaux, t0, tmax, order, abstol, params, parse_eqs)
         evaluate!(x, δt, x0) # new initial condition
-        tnext = t0+δt
-        # Evaluate solution at times within convergence radius
-        while t1 < tnext
-            evaluate!(x, t1-t0, x1)
-            @inbounds xv[:,iter] .= x1
-            iter += 1
-            @inbounds t1 = trange[iter]
-        end
-        if δt == tmax-t0
-            @inbounds xv[:,iter] .= x0
-            break
+        if dense
+            xv_interp[:,nsteps] .= deepcopy(x)
         end
         g_val = g(dx, x, params, t)
         nevents = TaylorIntegration.findroot!(t, x, dx, g_val_old, g_val, eventorder,
@@ -177,9 +162,11 @@ function apophisinteg(f!, g, q0::Array{U,1}, trange::AbstractVector{T},
         for i in eachindex(x0)
             @inbounds x[i][0] = x0[i]
         end
-        t0 = tnext
+        t0 += δt
         @inbounds t[0] = t0
         nsteps += 1
+        @inbounds tv[nsteps] = t0
+        @inbounds xv[:,nsteps] .= x0
         if nsteps > maxsteps
             @info("""
             Maximum number of integration steps reached; exiting.
@@ -187,6 +174,9 @@ function apophisinteg(f!, g, q0::Array{U,1}, trange::AbstractVector{T},
             break
         end
     end
-
-    return transpose(xv), view(tvS,1:nevents-1), view(transpose(view(xvS,:,1:nevents-1)),1:nevents-1,:), view(gvS,1:nevents-1)
+    if dense
+        return TaylorInterpolant(view(tv,1:nsteps), view(transpose(view(xv_interp,:,1:nsteps-1)),1:nsteps-1,:)), view(tvS,1:nevents-1), view(transpose(view(xvS,:,1:nevents-1)),1:nevents-1,:), view(gvS,1:nevents-1)
+    else
+        return view(tv,1:nsteps), view(transpose(view(xv,:,1:nsteps)),1:nsteps,:), view(tvS,1:nevents-1), view(transpose(view(xvS,:,1:nevents-1)),1:nevents-1,:), view(gvS,1:nevents-1)
+    end
 end
