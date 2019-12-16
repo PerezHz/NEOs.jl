f8546235be8b015f75780a45b9f402e162d85d87
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-12-15 23:54:04 -0600

    Update main.jl

--- main.jl
+++ main.jl
@@ -12,18 +12,20 @@ using TaylorSeries
 const varorder = 5 # varorder is the order corresponding to the jet transport perturbation
 const objname = "Apophis"
 const maxsteps = 10000
-const nyears = 5.0
+const nyears = -5.0
 const dense = true#false
 const apophisjlpath = dirname(pathof(Apophis))
-const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2012_2013.dat")
+# const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2012_2013.dat")
+const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2005_2006.dat")
 # const dynamics = RNp1BP_pN_A_J23E_J2S_ng_eph!
 const dynamics = RNp1BP_pN_A_J23E_J2S_ng_eph_threads!
 const t0 = datetime2julian(DateTime(2008,9,24,0,0,0)) #starting time of integration
 @show t0 == 2454733.5
 
 # path to local Solar System ephemeris file
-# ast_eph_file = joinpath(dirname(pathof(Apophis)), "../jpleph", "ss16ast343_eph_24yr_tx.jld")
-ast_eph_file = joinpath(dirname(pathof(Apophis)), "../jpleph", "ss16ast343_eph_5yr_tx.jld")
+# ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_24yr_tx.jld")
+# ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_5yr_tx.jld")
+ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_minus5yr_tx_BACKWARDS.jld")
 
 # dq: perturbation to nominal initial condition (Taylor1 jet transport)
 dq = Taylor1.(zeros(7), varorder)
@@ -35,12 +37,17 @@ dq[end][1] = 1e-14
 #     dq[i][1][i] = 1e-8
 # end
 # dq[end][1][end] = 1e-14
+# __dq = set_variables("ξ", order=varorder, numvars=6)
+# dq = vcat(__dq, 0__dq[1])
+# for i in 1:6
+#     dq[i][1][i] = 1e-8
+# end
 
 #integrator warmup
 propagate(objname, dynamics, 1, t0, nyears, ast_eph_file, output=false, dense=dense, dq=dq)
 println("*** Finished warmup")
 
-#propagate(objname, dynamics, 2, t0, nyears, ast_eph_file, dense=dense, dq=dq)
+#propagate(objname, dynamics, 5, t0, nyears, ast_eph_file, dense=dense, dq=dq)
 #println("*** Finished 2nd warmup")
 
 #root-finding methods warmup (integrate until first root-finding event):
