9f5b7281fb35e3e015ef3b556a34473c1ec8c389
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-12-21 20:30:46 -0600

    Update initial conditions subroutine; update main.jl

--- main.jl
+++ main.jl
@@ -11,64 +11,61 @@ using SPICE: furnsh
 
 #script parameters (TODO: use ArgParse.jl instead)
 const varorder = 5 # varorder is the order corresponding to the jet transport perturbation
+const nv = 7 #number of TaylorN variables
 const objname = "Apophis"
 const maxsteps = 10000
-const nyears = -5.0 #5.0 #24.0
+const nyears = 5.0 #-5.0 #24.0
 const dense = true #false
 const apophisjlpath = dirname(pathof(Apophis))
-# const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2012_2013.dat")
-const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2005_2006.dat")
+const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2012_2013.dat")
+# const radarobsfile = joinpath(apophisjlpath, "../Apophis_JPL_data_2005_2006.dat")
 # const dynamics = RNp1BP_pN_A_J23E_J2S_ng_eph!
 const dynamics = RNp1BP_pN_A_J23E_J2S_ng_eph_threads!
 const t0 = datetime2julian(DateTime(2008,9,24,0,0,0)) #starting time of integration
 @show t0 == 2454733.5
 
 # path to local Solar System ephemeris file
-# ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_24yr_tx.jld")
-# ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_5yr_tx.jld")
-ast_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_minus5yr_tx_BACKWARDS.jld")
-
+# ss_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_24yr_tx.jld")
+ss_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_5yr_tx.jld")
+# ss_eph_file = joinpath(apophisjlpath, "../jpleph", "ss16ast343_eph_minus5yr_tx_BACKWARDS.jld")
 
 # dq: perturbation to nominal initial condition (Taylor1 jet transport)
 dq = Taylor1.(zeros(7), varorder)
 dq[end][1] = 1e-14
 
-# dq: perturbation to nominal initial condition (TaylorN jet transport)
-# dq = set_variables("ξ", order=varorder, numvars=7)
+# # dq: perturbation to nominal initial condition (TaylorN jet transport)
+# dq = set_variables("ξ", order=varorder, numvars=nv)
 # for i in 1:6
 #     dq[i][1][i] = 1e-8
 # end
-# dq[end][1][end] = 1e-14
-# __dq = set_variables("ξ", order=varorder, numvars=6)
-# dq = vcat(__dq, 0__dq[1])
-# for i in 1:6
-#     dq[i][1][i] = 1e-8
+# if get_numvars() == 7
+#     dq[7][1][7] = 1e-14
 # end
 
 #integrator warmup
-propagate(objname, dynamics, 1, t0, nyears, ast_eph_file, output=false, dense=dense, dq=dq)
+propagate(objname, dynamics, 1, t0, nyears, ss_eph_file, output=false, dense=dense, dq=dq)
 println("*** Finished warmup")
 
-#propagate(objname, dynamics, 5, t0, nyears, ast_eph_file, dense=dense, dq=dq)
-#println("*** Finished 2nd warmup")
+propagate(objname, dynamics, 5, t0, nyears, ss_eph_file, dense=dense, dq=dq)
+println("*** Finished 2nd warmup")
 
 #root-finding methods warmup (integrate until first root-finding event):
-# propagate(objname, dynamics, 50, t0, nyears, ast_eph_file, dense=dense, dq=dq)
+# propagate(objname, dynamics, 50, t0, nyears, ss_eph_file, dense=dense, dq=dq)
 # println("*** Finished root-finding warmup")
 
-#propagate(objname, dynamics, 100, t0, nyears, ast_eph_file, dense=dense, dq=dq)
+#propagate(objname, dynamics, 100, t0, nyears, ss_eph_file, dense=dense, dq=dq)
 #println("*** Finished root-finding test: several roots")
 
 #Full jet transport integration until ~2038: about 8,000 steps
-propagate(objname, dynamics, maxsteps, t0, nyears, ast_eph_file, dense=dense, dq=dq, radarobsfile=radarobsfile)
-println("*** Finished full jet transport integration")
+#propagate(objname, dynamics, maxsteps, t0, nyears, ss_eph_file, dense=dense, dq=dq)
+#println("*** Finished full jet transport integration")
 
-# calculate computed values of time-delays and Doppler shifts
-ss16asteph, acc_eph, newtonianNb_Potential = Apophis.loadeph(ast_eph_file)
-astfname = "Apophis_jt.0.jld"
-t = load(astfname, "t")
-x = load(astfname, "x")
-tx = TaylorInterpolant(t, x)
-furnsh( joinpath(apophisjlpath, "../jpleph", "naif0012.tls") ) # load leapseconds kernel
-furnsh( joinpath(apophisjlpath, "../jpleph", "de431t.bsp") ) # at least one SPK file must be loaded to read .tls file
-Apophis.compute_radar_obs("deldop.jld", radarobsfile, tx, ss16asteph)
+# # calculate computed values of time-delays and Doppler shifts
+# ss16asteph, acc_eph, newtonianNb_Potential = Apophis.loadeph(ss_eph_file)
+# astfname = "Apophis_jt.0.jld"
+# t = load(astfname, "t")
+# x = load(astfname, "x")
+# tx = TaylorInterpolant(t, x)
+# furnsh( joinpath(apophisjlpath, "../jpleph", "naif0012.tls") ) # load leapseconds kernel
+# furnsh( joinpath(apophisjlpath, "../jpleph", "de431t.bsp") ) # at least one SPK file must be loaded to read .tls file
+# Apophis.compute_radar_obs("deldop.jld", radarobsfile, tx, ss16asteph)
--- src/initial_conditions.jl
+++ src/initial_conditions.jl
@@ -1,16 +1,21 @@
 function initialcond(dq::Vector=zeros(7))
-
-    @assert length(dq) == 7 "Perturbation `dq` to initial condition must have `length(dq) == 7`"
-
-    # output from JPL Horizons
-
-    q0 = Array{Float64}(undef, 7) #initial condition array
-
-    #99942 apophis
-    q0[1:3] .= [-0.9633018148154301, 0.5100291398744978, 0.1652803001584807]
-    q0[4:6] .= [-0.007118874645519014, -0.01206123416050721, -0.00466951380149129]
-
+    q0 = Vector{Float64}(undef, 7) #initial condition array
+    # output from JPL solution #199 SPK file for Apophis via SPICE.jl
+    q0[1:3] .= [-0.963307185415711, 0.5100200405384891, 0.16527677734731944]
+    q0[4:6] .= [-0.007118712801283596, -0.012061319251632138, -0.004669541303113223]
+    # # output from JPL solution #197 SPK file for Apophis via SPICE.jl
+    # q0[1:3] .= [-0.9633072659452289, 0.5100198812639655, 0.1652767211634811]
+    # q0[4:6] .= [-0.007118710212828963, -0.012061320780141318, -0.004669541846949655]
     q0[7] = 0.0 # A2 Yarkovsky coefficient
-
-    return q0 .+ dq
+    S = typeof(q0[1]+dq[1])
+    q = Vector{S}(undef, 7)
+    for i in 1:6
+        q[i] = q0[i] + dq[i]
+    end
+    if length(dq) == 7
+        q[7] = q0[7] + dq[7]
+    elseif length(dq) == 6
+        q[7] = q0[7] + 0dq[1]
+    end
+    return q
 end
