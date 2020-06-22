d3abe32d945e28124789f4a9de229ea7b869bcfe
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2020-06-22 01:02:35 -0500

    Update artifacts: use tarballs from AWS S3 (temporary solution)

--- dev/generate_artifacts.jl
+++ dev/generate_artifacts.jl
@@ -9,20 +9,32 @@ using Pkg
 Pkg.PlatformEngines.probe_platform_engines!()
 
 # This is the path to the Artifacts.toml we will manipulate
-artifact_toml = joinpath(pathof(Apophis), "Artifacts.toml")
+artifact_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
 
 ftp_jpl = "ftp://ssd.jpl.nasa.gov/pub/ssd/debias/"
 
-names = ["debias", "debias_2014", "debias_2018", "debias_hires2018"]
-urls =ftp_jpl .* names .* ".tgz"
+names_debias = ["debias", "debias_2014", "debias_2018", "debias_hires2018"]
+urls_debias = ftp_jpl .* names_debias .* ".tgz"
 
-for (url, name) in map((x,y)->(x,y), urls, names)
+names_lsk_spk = ["naif0012", "de430", "TTmTDBde430", "a99942"]
+urls_lsk_spk = [
+    "https://jpleph.s3-us-west-2.amazonaws.com/naif0012.tar.gz",
+    "https://jpleph.s3-us-west-2.amazonaws.com/de430.tar.gz",
+    "https://jpleph.s3-us-west-2.amazonaws.com/TTmTDBde430.tar.gz",
+    "https://jpleph.s3-us-west-2.amazonaws.com/a99942.tar.gz"
+]
+
+urls_ = vcat(urls_lsk_spk, urls_debias)
+names_ = vcat(names_lsk_spk, names_debias)
+
+for (url, name) in map((x,y)->(x,y), urls_, names_)
     # Query the `Artifacts.toml` file for the hash bound to the name
     # (returns `nothing` if no such binding exists)
     artfct_hash = artifact_hash(name, artifact_toml)
 
     # If the name was not bound, or the hash it was bound to does not exist, create it!
     if artfct_hash == nothing || !artifact_exists(artfct_hash)
+        tb = Tuple[]
         # create_artifact() returns the content-hash of the artifact directory once we're finished creating it
         artfct_hash = create_artifact() do artifact_dir
             # We create the artifact by simply downloading a few files into the new artifact directory
@@ -33,17 +45,17 @@ for (url, name) in map((x,y)->(x,y), urls, names)
                     bytes2hex(sha256(file))
                 end
                 @show tarball_hash
-                global tb = [(url, tarball_hash)]
-                ### unpack(tarball, joinpath(artifact_dir, name))
+                tb = [(url, tarball_hash)]
                 unpack(tarball, artifact_dir)
             finally
                 rm(tarball)
             end
         end
-        # Now bind that hash within our `Artifacts.toml`.  `force = true` means that if it already exists,
-        # just overwrite with the new content-hash.  Unless the source files change, we do not expect
-        # the content hash to change, so this should not cause unnecessary version control churn.
-        bind_artifact!(artifact_toml, name, artfct_hash, lazy=true, download_info=tb) ####, force=true
+        # Now bind that hash within our `Artifacts.toml`. `force = true` means that if it already exists,
+        # just overwrite the corresponding entry in `Artifacts.toml` with the new content-hash.
+        # Unless the source files change, we do not expect the content hash to
+        # change, so this should not cause unnecessary version control churn.
+        bind_artifact!(artifact_toml, name, artfct_hash, lazy=true, force=true, download_info=tb)
     end
 
     # Get the path of the artifact, either newly created or previously generated.
