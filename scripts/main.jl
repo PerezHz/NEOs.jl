20053530008fb50911f73d11b7d0518e216dd4f2
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2020-10-23 23:48:22 -0500

    Update main.jl (update file paths; remove old code)

--- scripts/main.jl
+++ scripts/main.jl
@@ -28,10 +28,10 @@ const t0 = 0.0 # integration initial time
 #### observation data files (ra/dec, del/dop)
 const opticalobsfile = ""
 const radarobsfile = ""
-#const opticalobsfile = joinpath(apophisjlpath, "tholen13_mpc_formatted.dat")
-#const radarobsfile = joinpath(apophisjlpath, "Apophis_JPL_data_2005_2006.dat")
-#const opticalobsfile = joinpath(apophisjlpath, "vokr15_mpc_formatted.dat")
-#const radarobsfile = joinpath(apophisjlpath, "Apophis_JPL_data_2012_2013.dat")
+#const opticalobsfile = joinpath(apophisjlpath, "data", "tholen13_mpc_formatted.dat")
+#const radarobsfile = joinpath(apophisjlpath, "data", "Apophis_JPL_data_2005_2006.dat")
+#const opticalobsfile = joinpath(apophisjlpath, "data", "vokr15_mpc_formatted.dat")
+#const radarobsfile = joinpath(apophisjlpath, "data", "Apophis_JPL_data_2012_2013.dat")
 
 # path to local Solar System ephemeris file
 #ss_eph_file = joinpath(apophisjlpath, "jldeph", "ss16ast343_eph_m5y_et.jld")
@@ -61,22 +61,3 @@ println("*** Finished warmup")
 ###propagate(objname, dynamics, maxsteps, t0, nyears, ss_eph_file, dense=dense, dq=dq) # no obs ephemeris computation
 propagate(objname, dynamics, maxsteps, t0, nyears, ss_eph_file, dense=dense, dq=dq, quadmath=quadmath, radarobsfile=radarobsfile, opticalobsfile=opticalobsfile, debias_table=debias_table)
 println("*** Finished full jet transport integration")
-
-
-
-
-
-#const ss_eph_file_m5y = joinpath(apophisjlpath, "jldeph", "ss16ast343_eph_m5y_et_f64.jld")
-#const ss_eph_file_p6y = joinpath(apophisjlpath, "jldeph", "ss16ast343_eph_p6y_et_f64.jld")
-#const astfname_m5y = "Apophis_jt_m5y_o5v7.jld"
-#const astfname_p6y = "Apophis_jt_p6y_o5v7.jld"
-#ss16asteph_m5y = load(ss_eph_file_m5y, "ss16ast_eph")
-#ss16asteph_p6y = load(ss_eph_file_p6y, "ss16ast_eph")
-#apophis_m5y = load(astfname_m5y, "apophis")
-#apophis_p6y = load(astfname_p6y, "apophis")
-#furnsh( joinpath(apophisjlpath, "jldeph", "naif0012.tls") ) # load leapseconds kernel
-#furnsh( joinpath(apophisjlpath, "jldeph", "de430_1850-2150.bsp") ) # at least one SPK file must be loaded to read .tls file
-#Apophis.compute_radar_obs("deldop_m5y_o5v7.jld", radarobsfile0506, apophis_m5y, ss16asteph_m5y)
-#Apophis.compute_radar_obs("deldop_p6y_o5v7.jld", radarobsfile1213, apophis_p6y, ss16asteph_p6y)
-#Apophis.compute_optical_obs("radec_tho13_o5v7.jld", tho13obsfile, apophis_m5y, ss16asteph_m5y)
-#Apophis.compute_optical_obs_v15("radec_vokr15_o5v7.jld", vokr15obsfile, apophis_p6y, ss16asteph_p6y)
