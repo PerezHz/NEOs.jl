062f28467f7cd245f04d53714247a1f938adee5d
Jorge A. Pérez Hernández <perez.hz@gmail.com>
2023-05-29 18:10:37 +0200

    Update artifacts generation script

--- dev/generate_artifacts.jl
+++ dev/generate_artifacts.jl
@@ -7,27 +7,25 @@
 # TO DO: adapt script for 2010 debiasing tables (requires extra unpacking)
 
 using Pkg.Artifacts
-using Pkg.PlatformEngines
-using Pkg.PlatformEngines: sha256
-using Pkg
-# using Downloads: download
-Pkg.PlatformEngines.probe_platform_engines!()
+using Pkg.PlatformEngines: sha256, unpack
+using Downloads
 
 # This is the path to the Artifacts.toml we will manipulate
 const artifact_toml = joinpath(dirname(@__DIR__), "Artifacts.toml")
 
 # JPL FTP URL
-const ftp_jpl = "ftp://ssd.jpl.nasa.gov/pub/ssd/debias/"
+const ftp_jpl = "https://ssd.jpl.nasa.gov/ftp/"
 
 # Debiasing tables names and URLs
 const names_debias = ["debias", "debias_2014", "debias_2018", "debias_hires2018"]
-const urls_debias = ftp_jpl .* names_debias .* ".tgz"
+const urls_debias = ftp_jpl .* "ssd/debias/" .* names_debias .* ".tgz"
 
 # JPL lsk and spk names and URLs
-const names_lsk_spk = ["naif0012", "de430", "a99942"]
+const names_lsk_spk = ["naif0012", "de430", "TTmTDBde430", "a99942"]
 const urls_lsk_spk = [
-    "https://raw.githubusercontent.com/PerezHz/jpleph/main/naif0012.tar.gz",
-    "https://raw.githubusercontent.com/PerezHz/jpleph/main/de430.tar.gz",
+    "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls",
+    ftp_jpl*"eph/planets/bsp/de430_1850-2150.bsp",
+    ftp_jpl*"eph/planets/bsp/TTmTDB.de430.19feb2015.bsp",
     "https://raw.githubusercontent.com/PerezHz/jpleph/main/a99942.tar.gz",
 ]
 
@@ -36,35 +34,45 @@ const name_sseph = "sseph_p100"
 const url_sseph = "https://github.com/LuEdRaMo/sseph/raw/main/sseph343ast016_p100y_et.tar.gz"
 
 # Collect names and URLs
-const urls_ = vcat(urls_lsk_spk, urls_debias, url_sseph)
-const names_ = vcat(names_lsk_spk, names_debias, name_sseph)
+const urls = vcat(urls_lsk_spk, urls_debias, url_sseph)
+const names = vcat(names_lsk_spk, names_debias, name_sseph)
 
-for (url, name) in map((x,y)->(x,y), urls_, names_)
+for (url, name) in zip(urls, names)
     # Query the `Artifacts.toml` file for the hash bound to the name
     # (returns `nothing` if no such binding exists)
     artfct_hash = artifact_hash(name, artifact_toml)
-
     # If the name was not bound, or the hash it was bound to does not exist, create it!
     if isnothing(artfct_hash) || !artifact_exists(artfct_hash)
         tb = Tuple[]
         # create_artifact() returns the content-hash of the artifact directory once we're finished creating it
         artfct_hash = create_artifact() do artifact_dir
             # We create the artifact by simply downloading a few files into the new artifact directory
-            @show url
+            @show url name
             tmp_dir = mktempdir()
-            tarball = download(url, joinpath(tmp_dir, basename(url)))
-            # @show tarball tmp_dir
-            try
-                tarball_hash = open(tarball) do file
+            basename_url = basename(url)
+            @show basename_url
+            dot_idx = findlast(isequal('.'), basename_url)
+            ext = basename_url[dot_idx+1:end]
+            if ext in ["gz", "tgz"]  # If artifact is a tarball with many files
+                tarball = Downloads.download(url, joinpath(tmp_dir, basename(url)))
+                try
+                    tarball_hash = open(tarball) do file
+                        bytes2hex(sha256(file))
+                    end
+                    unpack(tarball, artifact_dir)
+                    tb = [(url, tarball_hash)]
+                finally
+                    rm(tarball)
+                end
+            else # url containing a single non-tarball file
+                fpath = Downloads.download(url, joinpath(artifact_dir, basename(url)))
+                file_hash = open(fpath) do file
                     bytes2hex(sha256(file))
                 end
-                @show tarball_hash
-                tb = [(url, tarball_hash)]
-                unpack(tarball, artifact_dir)
-            finally
-                rm(tarball)
+                tb = [(url, file_hash)]
             end
         end
+        @show  artfct_hash typeof(artfct_hash)
         # Now bind that hash within our `Artifacts.toml`. `force = true` means that if it already exists,
         # just overwrite the corresponding entry in `Artifacts.toml` with the new content-hash.
         # Unless the source files change, we do not expect the content hash to
