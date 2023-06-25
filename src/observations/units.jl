1bc7afff8a2adc075ddc0c95bb915d99314b0a2a
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2023-06-25 21:47:03 +0200

    Compute UTC->TDB conversion in datetime2et using NEOs.ttmtdb ephemeris (avoid calling SPICE.str2et)

--- src/observations/units.jl
+++ src/observations/units.jl
@@ -24,29 +24,38 @@ end
     datetime2et(x::DateTime)
     datetime2et(x::T) where {T <: AbstractAstrometry}
 
-Retun the TDB seconds past the J2000 epoch.
-
-See also [`SPICE.str2et`](@ref).
+Convert a UTC `DateTime` to TDB seconds past the J2000 epoch.
 """
 function datetime2et(x::DateTime)
-    return str2et(string(x))
+    # UTC seconds since J2000.0 epoch
+    utc_seconds = Millisecond(x - DateTime(2000,1,1,12)).value/1000
+    # TAI - UTC
+    tai_utc = get_Δat(datetime2julian(x))
+    # TT - TAI
+    tt_tai = 32.184
+    # TT - UTC = (TT-TAI) + (TAI-UTC)
+    tt_utc = tt_tai +tai_utc
+    # TT seconds  = UTC seconds + (TT-UTC)
+    tt_seconds = utc_seconds + tt_utc
+    # TDB seconds = TT seconds + (TDB-TT)
+    return tt_seconds - ttmtdb_tt(tt_seconds)
 end
 
-datetime2et(x::T) where {T <: AbstractAstrometry} = datetime2et(x.date)
+datetime2et(x::AbstractAstrometry) = datetime2et(x.date)
 
 @doc raw"""
     et_to_200X(et::T) where {T <: Number}
 
 Convert `et` ephemeris seconds since J2000 to years `200X`.
 """
-et_to_200X(et::T) where {T <: Number} = 2000 + et/daysec/yr
+et_to_200X(et::Number) = 2000 + et/daysec/yr
 
 @doc raw"""
     days_to_200X(d::T) where {T <: Number}
 
 Convert `d` days since J2000 to years `200X`.
 """
-days_to_200X(d::T) where {T <: Number} = 2000 + d/yr
+days_to_200X(d::Number) = 2000 + d/yr
 
 @doc raw"""
     datetime_to_200X(x::DateTime)
