326d79c51e20285a87a98f8a6d76eaf9e2ced550
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2023-09-03 16:12:07 -0600

    Yet another adaptative maxsteps

--- src/orbit_determination/gauss_method.jl
+++ src/orbit_determination/gauss_method.jl
@@ -400,7 +400,11 @@ function adaptative_maxsteps(radec::Vector{RadecMPC{T}}) where {T <: AbstractFlo
     # Time difference [days]
     Δ_day = Δ_ms / 86_400_000
     # Adaptative maxsteps
-    return 35 + ceil(Int, 5*Δ_day/160)
+    if Δ_day <= 30
+        return ceil(Int, -7*Δ_day/3) + 100
+    else
+        return ceil(Int, (Δ_day + 360)/13)
+    end
 end
 
 for i in 1:2
