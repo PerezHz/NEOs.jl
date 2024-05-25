106c8cf163e2b43b62660f19086386740d45eeff
Luis Benet <benet@icf.unam.mx>
2024-05-25 08:43:24 -0600

    Add filter to gaussinitcond and clean-up

--- src/orbit_determination/orbit_determination.jl
+++ src/orbit_determination/orbit_determination.jl
@@ -12,12 +12,7 @@ Check whether `radec` is a single observational arc, i.e. no two consecutive obs
 are more than `arc` days apart. The function assumes `radec` is sorted.
 """
 function issinglearc(radec::Vector{RadecMPC{T}}, arc::Day = Day(30)) where {T <: AbstractFloat}
-    for i in 2:length(radec)
-        if date(radec[i]) - date(radec[i-1]) > arc
-            return false
-        end
-    end
-    return true
+    return all(diff(date.(radec)) .< arc)
 end
 
 @doc raw"""
@@ -42,27 +37,6 @@ end
 isgauss(sol::NEOSolution{T, T}) where {T <: AbstractFloat} = isgauss(sol.tracklets)
 
 @doc raw"""
-    adaptative_maxsteps(radec::Vector{RadecMPC{T}}) where {T <: AbstractFloat}
-
-Empirical upper bound for the number of steps used for propagation.
-"""
-function adaptative_maxsteps(radec::Vector{RadecMPC{T}}) where {T <: AbstractFloat}
-    #=
-    # Time difference [ms]
-    Δ_ms =  getfield(date(radec[end]) - date(radec[1]), :value)
-    # Time difference [days]
-    Δ_day = Δ_ms / 86_400_000
-    # Adaptative maxsteps
-    if Δ_day <= 30
-        return 55 - floor(Int, 5*Δ_day/6)
-    else
-        return ceil(Int, (Δ_day + 360)/13)
-    end
-    =#
-    return 500
-end
-
-@doc raw"""
     orbitdetermination(radec::Vector{RadecMPC{T}}, params::NEOParameters{T};
                        dynamics::D = newtonian!) where {T <: AbstractFloat, D}
 
@@ -79,8 +53,6 @@ function orbitdetermination(radec::Vector{RadecMPC{T}}, params::NEOParameters{T}
 
     # Allocate NEOSolution for output
     sol = zero(NEOSolution{T, T})
-    # Maximum number of steps
-    params = NEOParameters(params; maxsteps = adaptative_maxsteps(radec))
     # Eliminate observatories without coordinates
     filter!(x -> hascoord(observatory(x)), radec)
     # Cannot handle zero observations or multiple arcs
