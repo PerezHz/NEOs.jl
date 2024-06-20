bc438f93627a792fb4582c34b6864422a35e205c
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2024-06-20 17:38:40 -0600

    Update NEOsRecipesBaseExt.jl

--- ext/NEOsRecipesBaseExt.jl
+++ ext/NEOsRecipesBaseExt.jl
@@ -2,16 +2,17 @@ module NEOsRecipesBaseExt
 
 using RecipesBase
 using PlanetaryEphemeris: TaylorInterpolant
-using NEOs: OpticalResidual, AdmissibleRegion, NEOSolution, cte, ra, dec, boundary
+using NEOs: OpticalResidual, AdmissibleRegion, NEOSolution, cte, ra, dec, arboundary
 
 @recipe function f(res::AbstractVector{OpticalResidual{T, U}}) where {T <: Real, U <: Number}
     seriestype --> :scatter
     return cte.(ra.(res)), cte.(dec.(res))
 end
 
-@recipe function f(A::AdmissibleRegion{T}; N::Int = 100, ρscale::Symbol = :linear) where {T <: Real}
+@recipe function f(A::AdmissibleRegion{T}; boundary::Symbol = :outer,
+                   N::Int = 100, ρscale::Symbol = :linear) where {T <: Real}
     seriestype --> :path
-    ps = map(t -> boundary(A, t), LinRange(0, 3, N))
+    ps = map(t -> arboundary(A, t, boundary), LinRange(0, 3, N))
     xs, ys = first.(ps), last.(ps)
     if ρscale == :log
         xs .= log10.(xs)
