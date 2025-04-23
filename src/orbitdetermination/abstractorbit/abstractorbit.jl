a2b71d372d5c57f3b0fff965f39c434b3c730143
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2025-04-23 17:13:31 -0600

    Adapt preliminary orbit determination functions to the AbstractPreliminaryOrbit interface

--- src/orbitdetermination/abstractorbit/abstractorbit.jl
+++ src/orbitdetermination/abstractorbit/abstractorbit.jl
@@ -1,7 +1,7 @@
 @doc raw"""
     AbstractOrbit{T <: Real, U <: Number}
 
-Supertype for the asteroid orbits API.
+Supertype for the orbits API.
 """
 abstract type AbstractOrbit{T <: Real, U <: Number} end
 
@@ -26,13 +26,31 @@ function minmaxdates(orbit::AbstractOrbit)
     return min(t, minimum(first, dates)), max(t, maximum(last, dates))
 end
 
+# Timespan of the observation window in days
 function numberofdays(orbit::AbstractOrbit)
     t0, tf = minmaxdates(orbit)
     return (tf - t0).value / 86_400_000
 end
 
+# Assemble the vector of optical astrometry
+astrometry(orbit::AbstractOrbit) = astrometry(orbit.tracklets)
+
 # Evaluation in time method
 (orbit::AbstractOrbit)(t = epoch(orbit)) = t <= epoch(orbit) ? orbit.bwd(t) : orbit.fwd(t)
+
+# Initialize a vector of residuals consistent with orbit
+function init_residuals(::Type{V}, orbit::AbstractOrbit{T, U}) where {T <: Real,
+    U <: Number, V <: Number}
+    # Initialize vector of residuals
+    res = Vector{OpticalResidual{T, V}}(undef, nobs(orbit))
+    for i in eachindex(res)
+        ξ_α, ξ_δ = zero(V), zero(V)
+        @unpack w_α, w_δ, μ_α, μ_δ, outlier = orbit.res[i]
+        res[i] = OpticalResidual{T, V}(ξ_α, ξ_δ, w_α, w_δ, μ_α, μ_δ, outlier)
+    end
+
+    return res
+end
 
 # Target functions
 chi2(orbit::AbstractOrbit{T, U}) where {T <: Real, U <: Number} =
@@ -58,15 +76,112 @@ function critical_value(orbit::AbstractOrbit{T, U}) where {T <: Real, U <: Numbe
     return cdf(d, χ2)
 end
 
-function init_residuals(::Type{V}, orbit::AbstractOrbit{T, U}) where {T <: Real,
-    U <: Number, V <: Number}
-    # Initialize vector of residuals
-    res = Vector{OpticalResidual{T, V}}(undef, nobs(orbit))
-    for i in eachindex(res)
-        ξ_α, ξ_δ = zero(V), zero(V)
-        @unpack w_α, w_δ, μ_α, μ_δ, outlier = orbit.res[i]
-        res[i] = OpticalResidual{T, V}(ξ_α, ξ_δ, w_α, w_δ, μ_α, μ_δ, outlier)
-    end
+@doc raw"""
+    sigmas(::AbstractOrbit)
 
-    return res
+Return the uncertainties in barycentric cartesian coordinates at the reference epoch.
+"""
+sigmas(orbit::AbstractOrbit) = map(x -> x < 0 ? NaN * x : sqrt(x), variances(orbit))
+
+@doc raw"""
+    snr(::AbstractOrbit)
+
+Return the signal-to-noise ratios in barycentric cartesian
+coordinates at the reference epoch.
+"""
+snr(orbit::AbstractOrbit) = abs.(orbit()) ./ sigmas(orbit)
+
+@doc raw"""
+    jplcompare(des::String, orbit::AbstractOrbit)
+
+Return the absolute difference between `orbit` and JPL's orbit for object
+`des` computed in barycentric cartesian coordinates at `epoch(orbit)` in
+units of `sigmas(orbit)`.
+"""
+function jplcompare(des::String, orbit::AbstractOrbit)
+    # Load Solar System ephemerides
+    loadjpleph()
+    # orbit's initial condition
+    q1 = cte.(orbit())
+    # Time of first (last) observation
+    t0, tf = minmaxdates(orbit)
+    # Download JPL ephemerides
+    bsp = smb_spk("DES = $(des);", t0 - Minute(10), tf + Minute(10))
+    # Object ID
+    id = parse(Int, bsp[1:end-4])
+    # Load JPL ephemerides
+    furnsh(bsp)
+    # Delete ephemerides file
+    rm(bsp)
+    # JPL barycentric state vector
+    q2 = kmsec2auday(spkgeo(id, julian2etsecs(epoch(orbit) + PE.J2000),
+        "J2000", 0)[1])
+    # Absolute difference in sigma units
+    return @. abs(q1 - q2) / sigmas(orbit)
+end
+
+@doc raw"""
+    uncertaintyparameter(od, orbit, params) where {D, T <: Real}
+
+Return the Minor Planet Center uncertainty parameter.
+
+## Arguments
+
+- `od::ODProblem{D, T}`: an orbit determination problem.
+- `orbit::AbstractOrbit{T, T}`: a priori orbit.
+- `params::Parameters{T}`: see [`Parameters`](@ref).
+
+!!! reference
+    https://www.minorplanetcenter.net/iau/info/UValue.html
+
+!!! warning
+    This function will change the (global) `TaylorSeries` variables.
+"""
+function uncertaintyparameter(od::ODProblem{D, T}, orbit::AbstractOrbit{T, T},
+    params::Parameters{T}) where {D, T <: Real}
+    # Check consistency between od and orbit
+    @assert od.tracklets == orbit.tracklets
+    # Reference epoch [Julian days TDB]
+    t = epoch(orbit)
+    jd0 = t + PE.J2000
+    # Barycentric initial conditions
+    q00 = orbit(t)
+    # Scaling factors
+    scalings = all(>(0), variances(orbit)) ? sigmas(orbit) : @. abs(q00) / 1e6
+    # Jet transport variables
+    dq = scaled_variables("dx", scalings; order = 2)
+    # Origin
+    x0 = zeros(T, 6)
+    # Initial conditions
+    q0 = q00 + dq
+    # Propagation and residuals
+    res = init_residuals(TaylorN{T}, orbit)
+    propres!(res, od, jd0, q0, params)
+    nobs = 2 * notout(res)
+    # Covariance matrix
+    Q = nms(res)
+    C = (nobs/2) * TS.hessian(Q, x0)
+    Γ = inv(C)
+    # Osculating keplerian elements
+    osc = pv2kep(q0 - params.eph_su(t); jd = jd0, frame = :ecliptic)
+    # Semimajor axis [au], eccentricity and time of perihelion passage [julian days]
+    @unpack a, e, tp = osc
+    # Gauss gravitational constant [deg]
+    k_0 = 180 * k_gauss / π
+    # Orbital period [days]
+    P = 2π * sqrt(a^3 / μ_S)
+    # Projected covariance matrix
+    t_car2kep = TS.jacobian([tp, P], x0)
+    Γ_kep = t_car2kep * Γ * t_car2kep'
+    # Uncertainties
+    dtp, dP = sqrt.(diag(Γ_kep))
+    # Convert orbital period to years
+    P = P / yr
+    # In-orbit longitude runoff [arcsec / decade]
+    runoff = (dtp * e(x0) + 10 * dP / P(x0)) * (k_0 / P(x0)) * 3_600 * 3
+    # Uncertainty parameter
+    C = log(648_000) / 9
+    U = floor(Int, log(runoff) / C) + 1
+
+    return clamp(U, 0, 10)
 end
--- src/orbitdetermination/abstractorbit/preliminaryorbit.jl
+++ src/orbitdetermination/abstractorbit/preliminaryorbit.jl
@@ -1,7 +1,19 @@
 @doc raw"""
-    PreliminaryOrbit{T, U} <: AbstractOrbit{T, U}
+    AbstractPreliminaryOrbit{T, U} <: AbstractOrbit{T, U}
 
-An asteroid preliminary orbit.
+Supertype for the preliminary orbits API.
+"""
+abstract type AbstractPreliminaryOrbit{T, U} <: AbstractOrbit{T, U} end
+
+init_residuals(::Type{U}, od::ODProblem, ::AbstractPreliminaryOrbit) where {U <: Number} =
+    init_residuals(U, od)
+
+variances(orbit::AbstractPreliminaryOrbit) = diag(orbit.J * orbit.Γ * orbit.J')
+
+@doc raw"""
+    GaussOrbit{T, U} <: AbstractPreliminaryOrbit{T, U}
+
+A preliminary orbit computed by Gauss method.
 
 ## Fields
 
@@ -9,43 +21,198 @@ An asteroid preliminary orbit.
 - `bwd/fwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}`:
     backward (forward) integration.
 - `res::Vector{OpticalResidual{T, U}}`: vector of optical residuals.
+- `Γ::Matrix{T}`: covariance matrix.
+- `J::Matrix{T}`: residuals space to barycentric coordinates jacobian.
+- `τ_1/τ_3::T`: time between first (third) and second observations.
+- `ρ_vec::Matrix{U}`: topocentric line-of-sight unit vectors.
+- `R_vec::Matrix{T}`: observer's heliocentric positions.
+- `D_0::U`: D scalar.
+- `D::Matrix{U}`: D matrix.
+- `a/b/c::U`: lagrange polynomial coefficients.
+- `r_2::U`: lagrange polynomial root.
+- `r_vec::Matrix{U}`: heliocentric state vectors of the object.
+- `ρ::Vector{U}`: topocentric slant ranges.
 """
-@auto_hash_equals struct PreliminaryOrbit{T, U} <: AbstractOrbit{T, U}
+@auto_hash_equals struct GaussOrbit{T, U} <: AbstractPreliminaryOrbit{T, U}
     tracklets::Vector{Tracklet{T}}
     bwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}
     fwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}
     res::Vector{OpticalResidual{T, U}}
+    Γ::Matrix{T}
+    J::Matrix{T}
+    τ_1::T
+    τ_3::T
+    ρ_vec::Matrix{U}
+    R_vec::Matrix{T}
+    D_0::U
+    D::Matrix{U}
+    a::U
+    b::U
+    c::U
+    r_2::U
+    r_vec::Matrix{U}
+    ρ::Vector{U}
     # Inner constructor
-    function PreliminaryOrbit{T, U}(tracklets::Vector{Tracklet{T}},
+    function GaussOrbit{T, U}(tracklets::Vector{Tracklet{T}},
         bwd::TaylorInterpolant{T, U, 2}, fwd::TaylorInterpolant{T, U, 2},
-        res::Vector{OpticalResidual{T, U}}) where {T <: Real, U <: Number}
+        res::Vector{OpticalResidual{T, U}}, Γ::Matrix{T}, J::Matrix{T},
+        τ_1::T, τ_3::T, ρ_vec::Matrix{U}, R_vec::Matrix{T}, D_0::U,
+        D::Matrix{U}, a::U, b::U, c::U, r_2::U, r_vec::Matrix{U},
+        ρ::Vector{U}) where {T <: Real, U <: Number}
         @assert bwd.t0 == fwd.t0 "Backward and forward integration initial \
             times must match"
         @assert nobs(tracklets) == length(res) "Number of observations must \
             match number of residuals"
         _bwd_ = TaylorInterpolant(bwd.t0, bwd.t, collect(bwd.x))
         _fwd_ = TaylorInterpolant(fwd.t0, fwd.t, collect(fwd.x))
-        return new{T, U}(tracklets, _bwd_, _fwd_, res)
+        return new{T, U}(tracklets, _bwd_, _fwd_, res, Γ, J, τ_1, τ_3, ρ_vec,
+            R_vec, D_0, D, a, b, c, r_2, r_vec, ρ)
     end
 end
 
 # Outer constructor
-PreliminaryOrbit(tracklets::Vector{Tracklet{T}}, bwd::TaylorInterpolant{T, U, 2},
-    fwd::TaylorInterpolant{T, U, 2}, res::Vector{OpticalResidual{T, U}}) where
-    {T <: Real, U <: Number} = PreliminaryOrbit{T, U}(tracklets, bwd, fwd, res)
+GaussOrbit(tracklets::Vector{Tracklet{T}}, bwd::TaylorInterpolant{T, U, 2},
+    fwd::TaylorInterpolant{T, U, 2}, res::Vector{OpticalResidual{T, U}},
+    Γ::Matrix{T}, J::Matrix{T}, τ_1::T, τ_3::T, ρ_vec::Matrix{U},
+    R_vec::Matrix{T}, D_0::U, D::Matrix{U}, a::U, b::U, c::U, r_2::U,
+    r_vec::Matrix{U}, ρ::Vector{U}) where {T <: Real, U <: Number} =
+    GaussOrbit{T, U}(tracklets, bwd, fwd, res, Γ, J, τ_1, τ_3, ρ_vec,
+    R_vec, D_0, D, a, b, c, r_2, r_vec, ρ)
 
-# Print method for PreliminaryOrbit
-show(io::IO, orbit::PreliminaryOrbit) = print(io, "Preliminary orbit with ",
+# Print method for GaussOrbit
+show(io::IO, orbit::GaussOrbit) = print(io, "Gauss preliminary orbit with ",
     length(orbit.res), " residuals")
 
-# Definition of zero PreliminaryOrbit
-function zero(::Type{PreliminaryOrbit{T, U}}) where {T <: Real, U <: Number}
+# Definition of zero GaussOrbit
+function zero(::Type{GaussOrbit{T, U}}) where {T <: Real, U <: Number}
     tracklets = Vector{Tracklet{T}}(undef, 0)
     bwd = zero(TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}})
     fwd = zero(TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}})
     res = Vector{OpticalResidual{T, U}}(undef, 0)
-    return PreliminaryOrbit{T, U}(tracklets, bwd, fwd, res)
+    Γ = Matrix{T}(undef, 0, 0)
+    J = Matrix{T}(undef, 0, 0)
+    τ_1, τ_3 = zero(T), zero(T)
+    ρ_vec = Matrix{U}(undef, 0, 0)
+    R_vec = Matrix{T}(undef, 0, 0)
+    D_0 = zero(U)
+    D = Matrix{U}(undef, 0, 0)
+    a, b, c = zero(U), zero(U), zero(U)
+    r_2 = zero(U)
+    r_vec = Matrix{U}(undef, 0, 0)
+    ρ = Vector{U}(undef, 0)
+
+    return GaussOrbit{T, U}(tracklets, bwd, fwd, res, Γ, J, τ_1, τ_3, ρ_vec,
+        R_vec, D_0, D, a, b, c, r_2, r_vec, ρ)
 end
 
-iszero(orbit::PreliminaryOrbit{T, U}) where {T <: Real, U <: Number} =
-    orbit == zero(PreliminaryOrbit{T, U})
+iszero(orbit::GaussOrbit{T, U}) where {T <: Real, U <: Number} =
+    orbit == zero(GaussOrbit{T, U})
+
+# Evaluate TaylorN's in deltas
+function evaldeltas(orbit::GaussOrbit{T, TaylorN{T}}, δs::Vector{T} =
+    zeros(T, get_numvars())) where {T <: Real}
+    # Evaluate integrations
+    new_bwd_x = map(x -> Taylor1(x.coeffs(δs)), orbit.bwd.x)
+    new_bwd = TaylorInterpolant(orbit.bwd.t0, orbit.bwd.t, new_bwd_x)
+    new_fwd_x = map(x -> Taylor1(x.coeffs(δs)), orbit.fwd.x)
+    new_fwd = TaylorInterpolant(orbit.fwd.t0, orbit.fwd.t, new_fwd_x)
+    # Evaluate residuals
+    new_res = orbit.res(δs)
+    # Evaluate Gauss method objects
+    new_ρ_vec = orbit.ρ_vec(δs)
+    new_D_0 = orbit.D_0(δs)
+    new_D = orbit.D(δs)
+    new_a, new_b, new_c = orbit.a(δs), orbit.b(δs), orbit.c(δs)
+    new_r_2 = orbit.r_2(δs)
+    new_r_vec = orbit.r_vec(δs)
+    new_ρ = orbit.ρ(δs)
+
+    return GaussOrbit{T, T}(orbit.tracklets, new_bwd, new_fwd, new_res, orbit.Γ,
+        orbit.J, orbit.τ_1, orbit.τ_3, new_ρ_vec, orbit.R_vec, new_D_0, new_D,
+        new_a, new_b, new_c, new_r_2, new_r_vec, new_ρ)
+end
+
+@doc raw"""
+    MMOVOrbit{T, U} <: AbstractPreliminaryOrbit{T, U}
+
+A preliminary orbit computed by the minimization over the MOV method.
+
+## Fields
+
+- `tracklets::Vector{Tracklet{T}}`: vector of tracklets.
+- `bwd/fwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}`:
+    backward (forward) integration.
+- `res::Vector{OpticalResidual{T, U}}`: vector of optical residuals.
+- `Γ::Matrix{T}`: covariance matrix.
+- `J::Matrix{T}`: residuals space to barycentric coordinates jacobian.
+- `aes::Matrix{T}`: history of attributable elements.
+- `Qs::Vector{T}`: history of the target function.
+"""
+@auto_hash_equals struct MMOVOrbit{T, U} <: AbstractPreliminaryOrbit{T, U}
+    tracklets::Vector{Tracklet{T}}
+    bwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}
+    fwd::TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}}
+    res::Vector{OpticalResidual{T, U}}
+    Γ::Matrix{T}
+    J::Matrix{T}
+    aes::Matrix{T}
+    Qs::Vector{T}
+    # Inner constructor
+    function MMOVOrbit{T, U}(
+            tracklets::Vector{Tracklet{T}}, bwd::TaylorInterpolant{T, U, 2},
+            fwd::TaylorInterpolant{T, U, 2}, res::Vector{OpticalResidual{T, U}},
+            Γ::Matrix{T}, J::Matrix{T}, aes::Matrix{T}, Qs::Vector{T}
+        ) where {T <: Real, U <: Number}
+        @assert bwd.t0 == fwd.t0 "Backward and forward integration initial \
+            times must match"
+        @assert nobs(tracklets) == length(res) "Number of observations must \
+            match number of residuals"
+        # @assert size(aes, 1) == 6
+        # @assert size(aes, 2) == length(Qs)
+        _bwd_ = TaylorInterpolant(bwd.t0, bwd.t, collect(bwd.x))
+        _fwd_ = TaylorInterpolant(fwd.t0, fwd.t, collect(fwd.x))
+        return new{T, U}(tracklets, _bwd_, _fwd_, res, Γ, J, aes, Qs)
+    end
+end
+
+# Outer constructor
+MMOVOrbit(tracklets::Vector{Tracklet{T}}, bwd::TaylorInterpolant{T, U, 2},
+    fwd::TaylorInterpolant{T, U, 2}, res::Vector{OpticalResidual{T, U}},
+    Γ::Matrix{T}, J::Matrix{T}, aes::Matrix{T}, Qs::Vector{T}) where {T <: Real,
+    U <: Number} = MMOVOrbit{T, U}(tracklets, bwd, fwd, res, Γ, J, aes, Qs)
+
+# Print method for MMOVOrbit
+show(io::IO, orbit::MMOVOrbit) = print(io, "Minimization over the MOV preliminary orbit ",
+    "with ", length(orbit.res), " residuals")
+
+# Definition of zero MMOVOrbit
+function zero(::Type{MMOVOrbit{T, U}}) where {T <: Real, U <: Number}
+    tracklets = Vector{Tracklet{T}}(undef, 0)
+    bwd = zero(TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}})
+    fwd = zero(TaylorInterpolant{T, U, 2, Vector{T}, Matrix{Taylor1{U}}})
+    res = Vector{OpticalResidual{T, U}}(undef, 0)
+    Γ = Matrix{T}(undef, 0, 0)
+    J = Matrix{T}(undef, 0, 0)
+    aes = Matrix{T}(undef, 0, 0)
+    Qs = Vector{T}(undef, 0)
+
+    return MMOVOrbit{T, U}(tracklets, bwd, fwd, res, Γ, J, aes, Qs)
+end
+
+iszero(orbit::MMOVOrbit{T, U}) where {T <: Real, U <: Number} =
+    orbit == zero(MMOVOrbit{T, U})
+
+# Evaluate integrations and residuals in deltas
+function evaldeltas(orbit::MMOVOrbit{T, TaylorN{T}}, δs::Vector{T} =
+    zeros(T, get_numvars())) where {T <: Real}
+    # Evaluate integrations
+    new_bwd_x = map(x -> Taylor1(x.coeffs(δs)), orbit.bwd.x)
+    new_bwd = TaylorInterpolant(orbit.bwd.t0, orbit.bwd.t, new_bwd_x)
+    new_fwd_x = map(x -> Taylor1(x.coeffs(δs)), orbit.fwd.x)
+    new_fwd = TaylorInterpolant(orbit.fwd.t0, orbit.fwd.t, new_fwd_x)
+    # Evaluate residuals
+    new_res = orbit.res(δs)
+
+    return MMOVOrbit{T, T}(orbit.tracklets, new_bwd, new_fwd, new_res,
+        orbit.Γ, orbit.J, orbit.aes, orbit.Qs)
+end
