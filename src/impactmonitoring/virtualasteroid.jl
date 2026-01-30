0efd5cd92ad7a58e87697570cc40abb8feec7445
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2026-01-30 17:06:45 -0600

    Move truncation from CloseApproach to VirtualAsteroid

--- src/impactmonitoring/virtualasteroid.jl
+++ src/impactmonitoring/virtualasteroid.jl
@@ -19,12 +19,18 @@ end
 
 numtypes(::VirtualAsteroid{T, U}) where {T, U} = T, U
 
+# This function assumes that order ≤ get_order(x)
+reduceorder(x::Taylor1, order::Int) = Taylor1(x[0:order], order)
+
 # Outer constructors
 VirtualAsteroid(epoch::T, σ::T, domain::NTuple{2, T}, q0::Vector{U}) where {T, U} =
     VirtualAsteroid{T, U}(epoch, σ, domain, q0)
 
-VirtualAsteroid(lov::LineOfVariations, σ::Real, domain::NTuple{2, <:Real}) =
-    VirtualAsteroid(epoch(lov), σ, domain, lov(σ, domain))
+function VirtualAsteroid(lov::LineOfVariations, σ::Real,
+                         domain::NTuple{2, <:Real}, vaorder::Int)
+    @assert 0 ≤ vaorder ≤ get_order(lov)
+    return VirtualAsteroid(epoch(lov), σ, domain, reduceorder.(lov(σ, domain), vaorder))
+end
 
 # Print method for VirtualAsteroid
 function show(io::IO, x::VirtualAsteroid)
@@ -40,14 +46,16 @@ nominaltime(x::VirtualAsteroid) = x.epoch
 sigma(x::VirtualAsteroid) = x.σ
 initialcondition(x::VirtualAsteroid) = x.q0
 
-get_order(x::VirtualAsteroid{T, U}) where {T, U <: AbstractSeries} = get_order(first(x.q0))
+get_order(x::VirtualAsteroid{T, U}) where {T, U <: AbstractSeries} =
+    get_order(first(x.q0))
 
 """
-    virtualasteroids(lov, method; kwargs...)
+    virtualasteroids(lov, method [, vaorder]; kwargs...)
 
-Divide the line of variations `lov` into a vector of virtual asteroids
-using `method`. Each of the following available methods have their own
-keyword arguments:
+Divide the line of variations `lov` into a vector of `Taylor1` virtual
+asteroids using a given sampling `method` and truncating the polynomials
+to degree `vaorder` (default: `get_order(lov)`). Each of the following
+available methods have their own keyword arguments:
 - `:uniform`: domains of equal length. Keyword arguments:
     - `N::Int`: number of domains.
 - `:normal`: domains of equal probability according to a normal density
@@ -60,20 +68,20 @@ keyword arguments:
     - `IP::Real`: generic completeness limit.
     - `Δσmax`: maximum interval length.
 """
-virtualasteroids(lov::LineOfVariations, method::Symbol; kwargs...) =
-    virtualasteroids(Val(method), lov; kwargs...)
+virtualasteroids(lov::LineOfVariations, method::Symbol, vaorder::Int = get_order(lov);
+    kwargs...) = virtualasteroids(Val(method), lov, vaorder; kwargs...)
 
-function virtualasteroids(::Val{:uniform}, lov::LineOfVariations; N::Int)
+function virtualasteroids(::Val{:uniform}, lov::LineOfVariations, vaorder::Int; N::Int)
     endpoints = LinRange(lbound(lov), ubound(lov), N+1)
     domains = [(endpoints[i], endpoints[i+1]) for i in 1:N]
     σs = midpoint.(domains)
     if isodd(N)
         σs[(N ÷ 2) + 1] = sigma(lov)
     end
-    return VirtualAsteroid.(Ref(lov), σs, domains)
+    return VirtualAsteroid.(Ref(lov), σs, domains, Ref(vaorder))
 end
 
-function virtualasteroids(::Val{:normal}, lov::LineOfVariations; N::Int)
+function virtualasteroids(::Val{:normal}, lov::LineOfVariations, vaorder::Int; N::Int)
     _, T = numtypes(lov)
     d = Normal{T}(zero(T), one(T))
     xs = LinRange(cdf(d, lbound(lov)), cdf(d, ubound(lov)), N+1)
@@ -84,10 +92,10 @@ function virtualasteroids(::Val{:normal}, lov::LineOfVariations; N::Int)
     if isodd(N)
         σs[(N ÷ 2) + 1] = sigma(lov)
     end
-    return VirtualAsteroid.(Ref(lov), σs, domains)
+    return VirtualAsteroid.(Ref(lov), σs, domains, Ref(vaorder))
 end
 
-function virtualasteroids(::Val{:DelVigna19}, lov::LineOfVariations;
+function virtualasteroids(::Val{:DelVigna19}, lov::LineOfVariations, vaorder::Int;
                           R_TP::Real, R_P::Real, IP::Real, Δσmax::Real)
     _, T = numtypes(lov)
     σmax = ubound(lov)
@@ -116,5 +124,5 @@ function virtualasteroids(::Val{:DelVigna19}, lov::LineOfVariations;
     if isodd(N)
         σs[(N ÷ 2) + 1] = sigma(lov)
     end
-    return VirtualAsteroid.(Ref(lov), σs, domains)
+    return VirtualAsteroid.(Ref(lov), σs, domains, Ref(vaorder))
 end
