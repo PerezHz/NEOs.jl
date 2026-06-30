830dbdd34b6f027c0b5b05ef3b39ab695cbe4143
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2026-06-30 16:45:03 -0400

    Define print_mpec_residuals

--- src/orbitdetermination/abstractorbit/abstractorbit.jl
+++ src/orbitdetermination/abstractorbit/abstractorbit.jl
@@ -579,6 +579,9 @@ section of [`Parameters`](@ref).
 mass(orbit::AbstractOrbit, params::Parameters) =
     mass(params.density, diameter(orbit, params))
 
+print_mpec_residuals(x::AbstractOrbit) = print_mpec_residuals(x.optical, x.ores)
+print_mpec_residuals(io::IO, x::AbstractOrbit) = print_mpec_residuals(io, x.optical, x.ores)
+
 function summary(orbit::AbstractOrbit)
     O = nameof(typeof(orbit))
     T, U = numtypes(orbit)
